8988b4bf3feed759ea8c6d4c595c4df5dba17b81
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2019-11-15 18:55:00 +0200

    File Structuring
    File Structuring

--- src/containers/Example.js
+++ src/containers/Example.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Paginator from "Paginator";
+import Paginator from "../paginator/Paginator";
 
 export default class Example extends Component {
   constructor(props) {
@@ -25,7 +25,7 @@ export default class Example extends Component {
     const paginator = (
       <Paginator
         allRecords={items}
-        pageLimit={3}
+        pageLimit={1}
         currentPage={1}
         pageNeighbours={1}
         onPageChanged={this.onPageChanged}
@@ -33,7 +33,10 @@ export default class Example extends Component {
     );
 
     return (
-      <div style={{ marginTop: "10px", marginRight: "15px" }}>{paginator}</div>
+      <div style={{ marginTop: "10px", marginRight: "15px" }}>
+        <h1> Paginator Example</h1>
+        {paginator}
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Example from "./containers/Example";
+import "./assets/css/Paginator.css";
 
 console.log("Hello brudar");
 
